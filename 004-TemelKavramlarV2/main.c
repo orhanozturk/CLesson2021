f8e7341dbde89b5d982118f4124a48ceeb27eec4
OrhanOZTURK <orhn.ozturk@gmail.com>
2021-12-02 20:52:29 +0300

    Temelkavramlar ve veri türleri

---
+++ 004-TemelKavramlarV2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int main()
+{
+    printf("sizeof(_Bool) =  %zu\n", sizeof(_Bool));
+    printf("sizeof(char) =  %zu\n", sizeof(char));
+    printf("sizeof(short) =  %zu\n", sizeof(short));
+    printf("sizeof(int) =  %zu\n", sizeof(int));
+    printf("sizeof(long) =  %zu\n", sizeof(long));
+    printf("sizeof(long long) =  %zu\n", sizeof(long long));
+    printf("sizeof(float) =  %zu\n", sizeof(float));
+    printf("sizeof(double) =  %zu\n", sizeof(double));
+    printf("sizeof(long double) =  %zu\n", sizeof(long double));
+}
+
+
+
