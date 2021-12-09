bd9a49f7e7b9cce282b955323ae7ff4aa1d32b5e
OrhanOZTURK <orhn.ozturk@gmail.com>
2021-12-09 20:51:43 +0300

    ilkdegerVerme Scope Namelookup

---
+++ 006-ilkdegerVerme-Scope/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// scope (kapsam)
+
+//kısa bir mulakat sorusu
+
+//tanımsız davranıs
+
+int a = 10;
+
+int main()
+{
+    int a = a; // tanımsız davranıs
+
+    printf("a = %d\n", a);
+
+  return 0;
+}
+
+
+/**************************************************/
+/**************************************************/
