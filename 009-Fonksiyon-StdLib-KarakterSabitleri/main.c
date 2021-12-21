8699ef7eece0da97e910f20449b362fd69c570b6
OrhanOZTURK <orhn.ozturk@gmail.com>
2021-12-21 22:32:06 +0300

    Fonsiyon cagırma ve standart kutuphane ve Contant lar

---
+++ 009-Fonksiyon-StdLib-KarakterSabitleri/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a = 025;
+    int b = 25;
+    int c = 0x25;
+
+    printf("a = %d \n", a);
+    printf("b = %d \n", b);
+    printf("c = %d \n", c);
+
+    return 0;
+}
+
+/*******************************************/
+/*******************************************/
