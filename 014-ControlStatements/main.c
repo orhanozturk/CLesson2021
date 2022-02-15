fb914015f6b5691b1daef6b0d58222a927e58d1d
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-02-15 23:58:44 +0300

    loop statements

--- 014-ControlStatements/main.c
+++ 014-ControlStatements/main.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-#include <conio.h>
 
 int main()
 {
-    int c;
+    for(int i = 1000; i < 10000; ++i){
 
-    printf("evet mi (e) hayir mi (h) : ");
+        int d1 = i / 1000;
+        int d2 = i / 100 % 10;
+        int d3 = i / 10 % 10;
+        int d4 = i % 10;
 
+        if(i == d1*d1*d1*d1 + d2*d2*d2*d2 + d3*d3*d3*d3 + d4*d4*d4*d4)
+            printf("%d\n", i);
 
-    while((c = _getch()) != 'e' && c != 'h')
-        ;// null statment
+    }
 
-    putchar(c);
-    putchar('\n');
-
-
-    return 0;
 }
 
 /*****************************************/
