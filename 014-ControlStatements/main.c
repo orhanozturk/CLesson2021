e11592ea0211f7c02439985c32781ad4d0a61bf5
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-02-18 00:32:56 +0300

    loops

--- 014-ControlStatements/main.c
+++ 014-ControlStatements/main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
+
 int main()
 {
-    for(int i = 1000; i < 10000; ++i){
 
-        int d1 = i / 1000;
-        int d2 = i / 100 % 10;
-        int d3 = i / 10 % 10;
-        int d4 = i % 10;
-
-        if(i == d1*d1*d1*d1 + d2*d2*d2*d2 + d3*d3*d3*d3 + d4*d4*d4*d4)
-            printf("%d\n", i);
-
-    }
+    func();
+    //derleyici namelookup -- berlirli alanlarda ismi araması yapıyor
+    //bulmazsa sentaks hatası oluyor
+    //c++ da kesin olarak hata
 
 }
 
