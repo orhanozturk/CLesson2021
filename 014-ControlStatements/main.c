be9e61d5b2670b1bd352fdcc5d24768f0f94edaf
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-02-25 00:04:46 +0300

    Onislemci komutları

--- 014-ControlStatements/main.c
+++ 014-ControlStatements/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
-#include "utility.h"
+#include <ctype.h>
 
+//fonksiyonel macrolar
+
+#define     ISLEAP(x)       ( (x) % 4 == 0 && ( (x) % 100 || (x) % 400 == 0 ) )
 
 int main()
 {
+    int y;
+    printf("yil giriniz : ");
+    scanf("%d", &y);
+
+    if(ISLEAP(y)){
+        printf("artik yil\n");
+    }
+    else {
+        printf("artik yil deildir\n");
+    }
 
 
 
--- 014-ControlStatements/utility.h
+++ 014-ControlStatements/utility.h
@@ -1,6 +1,8 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#define MAX        100
+
 int isprime(int);
 
 #endif // UTILITY_H
