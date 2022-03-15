ba1439d212fd9ae3453b92390e71feaf6cc4ba2f
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-03-15 22:19:41 +0300

    Type Castting

---
+++ 015-GeneralCProgramming/main.c
@@ -0,0 +1,23 @@
+#include "utility.h"
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    //ufak kayıplar yasanır dikkatli olun
+    //boyle buyuk sayılarda
+
+    float dval = 20000001;
+
+    printf("dval = %f\n", dval);
+
+    return 0;
+}
+
+/****************************************************/
+/****************************************************/
+
+
+
+
+
