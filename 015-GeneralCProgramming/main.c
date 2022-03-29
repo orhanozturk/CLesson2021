39635758f37ac1206d12aad7d1b0527bf15aabb5
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-03-30 00:38:28 +0300

    Array and Search Algorithm

--- 015-GeneralCProgramming/main.c
+++ 015-GeneralCProgramming/main.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include "utility.h"
+#include <math.h>
 
-#define     SIZE        100
+#define     SIZE        10
 
 int main()
 {
     int a[SIZE];
 
     randomize();
-    set_array_random(a, SIZE);
+    for(int i = 0; i < SIZE; ++i){
+        a[i] = rand() % 20 + 1;
+    }
+
     print_array(a, SIZE);
 
+    for (int i = 0; i < SIZE; ++i) {
+        for (int k = 0; k < a[i] ; ++k) {
+            putchar('*');
+        }
+        putchar('\n');
+    }
 
     return 0;
 }
