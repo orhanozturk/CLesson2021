37c9898f0e436843fd33b8e50cf69f0b882fcdfe
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-04-16 12:59:56 +0300

    Poniter intro

--- 015-GeneralCProgramming/main.c
+++ 015-GeneralCProgramming/main.c
@@ -1,48 +1,19 @@
 #include <stdio.h>
 #include "utility.h"
-#include <ctype.h>
 
-#define     SIZE        20
 
-int is_than(int x)
-{
-    return x < 500;
-}
 
 int main()
 {
-    int a[SIZE];
+    int x = 10;
 
-    randomize();
-    set_array_random(a, SIZE);
-    print_array(a, SIZE);
 
-    int i;
+    printf("%zu\n", sizeof x * 5);
+    printf("%zu\n", sizeof (x * 5));
+    printf("%zu\n", sizeof (x) * 5);
 
-    for (i = 0; i < SIZE && is_than(a[i]); ++i)
-        ;
-
-    int k;
-
-    if(i == SIZE){
-        printf("butun ogeler kosulu salıyor\n");
-    }
-    else{
-        for (k = i+1; k < SIZE; ++k) {
-            if (is_than(a[k])) {
-                int temp = a[i];
-                a[i] = a[k];
-                a[k] = temp;
-                ++i;
-            }
-        }
-    }
-
-    printf("partisyon noktasi indxsi = %d\n", i);
-    print_array(a, SIZE);
-
-    return 0;
 }
+
 
 
 /****************************************************/
