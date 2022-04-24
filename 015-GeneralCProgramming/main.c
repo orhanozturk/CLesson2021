d82b8e84945056b1d0df8d0443f246ee7457dbaa
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-04-24 13:05:32 +0300

    const and pointer

--- 015-GeneralCProgramming/main.c
+++ 015-GeneralCProgramming/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include "utility.h"
 
+#define     SIZE        20
+
 int main()
 {
-    int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int a[SIZE];
 
-    for(int i = 0; i < 10; ++i){
-        printf("%d    %d\n", a[i], i[a]);
-        //*(a + i)  ---- *(i + a)
-    }
+    set_array_random(a, SIZE);
+    print_array(a, SIZE);
+    print_array(a, 5); //ilk 5 elemanı
+    print_array(a + 5, 3); //5 index dahil ile 8 index arasını yazdıracam
+    print_array(&a[5], 3); //5 index dahil  ile 8 index arasını yazdıracam
+    print_array(a + SIZE -4, 4); //son 4 elemanı yazdıracam
+    print_array(&a[SIZE -4], 4); //son 4 elemanı yazdıracam
 
 }
 
