b1b10c06ce4b6c87ee939364d8dfbd3f8051a062
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-05-15 11:10:14 +0300

    string islevleri inceleme

--- 015-GeneralCProgramming/main.c
+++ 015-GeneralCProgramming/main.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
 #include "utility.h"
-
+#include <string.h>
+#include <ctype.h>
 
 #define     SIZE        100
 
+//stricmp
+//kendimiz yazalım
+
+int mystricmp(const char *p1, const char *p2)
+{
+    while (toupper(*p1) == toupper(*p2)) {
+        if(*p1 == '\0')
+            return 0;
+        ++p1;
+        ++p2;
+    }
+
+
+    return toupper(*p1) - toupper(*p2);
+}
+
+
 int main()
 {
-   char str[SIZE];
+    char s1[SIZE];
+    char s2[SIZE];
 
-   printf("bir yazi giriniz : ");
-   sgets(str);
+    printf("birinci yaziyi girin : ");
+    sgets(s1);
 
-   print_str(str);
+    printf("ikinci yaziyi girin : ");
+    sgets(s2);
+
+    if(!mystricmp(s1, s2))
+        printf("evet \n");
+    else
+        printf("hayir\n");
+
 
 }
 
