fbd6f8d031901ca070fbd5ab0a7217134d1dd131
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-06-19 11:30:37 +0300

    functions pointers

--- 015-GeneralCProgramming/main.c
+++ 015-GeneralCProgramming/main.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include "utility.h"
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-#define     SIZE        10
 
+#define     SIZE        100
+
+int cmpfunc(const void *vpx, const void *vpy)
+{
+    if(*(const int *)vpx > *(const int *)vpy)
+        return 1;
+
+    if(*(const int *)vpx < *(const int *)vpy)
+        return -1;
+
+    return 0;
+}
+
+void gbsort(void *vpa, size_t size, size_t sz, int (*fcmp)(const void *, const void *))
+{
+    char *p = vpa;
+
+    for (size_t i = 0; i < size -1; ++i) {
+        for (size_t k = 0; k < size -1 -i; ++k) {
+            if(fcmp(p+k*sz, p+(k+1)*sz) > 0)
+                gswap(p+k*sz, p+(k+1)*sz, sz);
+        }
+    }
+
+}
 
 int main()
 {
-    int x = 10;
-    int y = 20;
+    int a[SIZE];
+    randomize();
+    set_array_random(a, SIZE);
+    print_array(a, SIZE);
 
-    void *vp= &x;
+    gbsort(a, SIZE, sizeof (int), &cmpfunc);
 
-    void **vptr = &vp;
-    *vptr = &y; //vp = &y
-
-
-    printf("val = %d\n", *(int *)vp);
-
+    print_array(a, SIZE);
 }
 
 
