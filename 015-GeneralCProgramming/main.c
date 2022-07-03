34d05146d64ec29437b2f204ef06febd4b5ca8e4
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-07-03 11:26:58 +0300

    exit abort dinamik bellek yonetimi

--- 015-GeneralCProgramming/main.c
+++ 015-GeneralCProgramming/main.c
@@ -1,23 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utility.h"
+#include <string.h>
 
-//mulakat sorusu
+#define SIZE    100
+
+int icmp(const void *vp1, const void *vp2)
+{
+    return *(const int *)vp1 - *(const int *)vp2;
+}
+
+int get_median(const int *p, size_t size)
+{
+    int *pd = malloc(size * sizeof (int));
+
+    if(!pd){
+        printf("bellek yetrsiz\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memcpy(pd, p, size * sizeof (int));
+    qsort(pd, size, sizeof (*pd), &icmp);
+
+    //bunu sakın sakına ypamayın bellek sızıntısı olur
+    return pd[size / 2];
+
+    //alınan bloklar eri verilmeli
+
+    //yapmamız geren işlem bu
+    /*
+    int result = pd[size / 2];
+
+    free(pd);
+
+    return result;
+    */
+}
 
 int main()
 {
-    int a[10][5] = {
-        [4] = {1, 2, 3, 4, 5},
-        [7] = {2, 2, 1, 2, 3},
-        [9] = {5, 5, 5, 5, 5},
-    };
+    int a[SIZE];
 
-    for (int i = 0; i < 10; ++i) {
-        for (int k = 0; k < 5; ++k) {
-            printf("%d ", a[i][k]);
-        }
-        printf("\n");
-    }
+    randomize();
+    set_array_random(a, SIZE);
+    print_array(a, SIZE);
+
+    int median = get_median(a, SIZE);
+    printf("median = %d\n", median);
+
 }
 
 
