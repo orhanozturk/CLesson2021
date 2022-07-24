99457fc90c13b4695985c335d58fdfe9216b5105
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-07-24 11:51:53 +0300

    dinamik bellek yonetimi ve dinamik dizi veri yapsıı

--- 015-GeneralCProgramming/main.c
+++ 015-GeneralCProgramming/main.c
@@ -2,50 +2,22 @@
 #include <stdlib.h>
 #include "utility.h"
 #include <string.h>
+#include <conio.h>
 
 #define SIZE    100
 
 
-int *create_random_array(size_t n, int low, int high)
-{
-    int *pd = (int *)malloc(n * sizeof (int));
-
-    if(pd == NULL){
-        printf("bellek yetersiz\n");
-        exit(EXIT_FAILURE);
-    }
-
-    for (size_t i = 0; i < n; ++i) {
-        pd[i] = rand() % (high - low + 1);
-    }
-
-    return pd;
-}
-
 
 int main()
 {
-    size_t n;
-    int low, high;
+    register int x = 10;
 
-    printf("kac elemanli dizi : ");
-    scanf("%zu", &n);
-    printf("aralik degerleri : ");
-    scanf("%d%d", &low, &high);
-
-    int *pd = create_random_array(n, low, high);
-    print_array(pd, n);
-    sort_array(pd, n);
-    dashline();
-    print_array(pd, n);
-    free(pd);
-
+    int *p = &x; //atayamazsınız register ile olsutrdugunuz nesneyi
 }
 
 
 /****************************************************/
 /****************************************************/
-
 
 
 
