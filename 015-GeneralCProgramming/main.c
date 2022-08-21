1c66189921705e8f7f44ff1b7854cb8bf27309e4
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-08-21 12:21:36 +0300

    struct - yapılar

--- 015-GeneralCProgramming/main.c
+++ 015-GeneralCProgramming/main.c
@@ -2,14 +2,37 @@
 #include "utility.h"
 #include <string.h>
 
+typedef struct Employee{
+    int id;
+    char name[20];
+    char surname[30];
+    double wage;
+}Employee;
 
 
+//typedef struct Employee Employee;
+
+void set_employee(Employee *e) //setter
+{
+    e->id = 123123;
+    e->wage = 111.;
+    _strupr(e->name);
+    _strupr(e->surname);
+}
+
+
+void print_employee(const Employee *e) //getter
+{
+    printf("%d  %s  %s  %.2f\n", e->id, e->name, e->surname, e->wage);
+}
 
 int main()
 {
+    struct Employee emp = {20, "orhan", "ozturk", 45.3};
 
-    memcpy_s()
-
+    printf("%d  %s  %s  %.2f\n", emp.id, emp.name, emp.surname, emp.wage);
+    set_employee(&emp);//call by reference
+    print_employee(&emp);//call by reference
 
 }
 
